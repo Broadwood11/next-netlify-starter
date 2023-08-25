c4f44919ea234374b1b45f854b84a46f3bf1e1ee
Broadwood11 <108820251+Broadwood11@users.noreply.github.com>
2023-08-24 20:55:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Footer from '@components/Footer';
 import { useState } from 'react';
 
 export default function Home() {
+
+  const conversation_id = "X46dBNq2Jd79";
+  const access_token = 'kRkX76Nm8u2Og6ZoZGct4KmprDZ1PkjMVJXK1IYa';
   // State to hold the form input
   const [formData, setFormData] = useState({
     firstname: '',
@@ -13,6 +16,31 @@ export default function Home() {
     // Add other fields as needed
   });
 
+  const get_answer = async (question) => {
+      const url = 'https://getcody.ai/api/v1/messages';
+      const headers = {
+        'Authorization': `Bearer ${access_token}`,
+        'Content-Type': 'application/json',
+      };
+      const payload = {
+        content: question,
+        conversation_id: conversation_id,
+      };
+
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(payload),
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        return data.data.content;
+      } else {
+        console.error('Error:', response.status);
+        return 'Error fetching answer';
+      }
+    };
   // State to hold the generated email content
   const [emailContent, setEmailContent] = useState('');
 
@@ -26,14 +54,20 @@ export default function Home() {
   };
 
   // Function to handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Generate the question
+    const question = `write a bullet point for use in an email highlighting how IGEL and Lenovo can help with ${formData.intent1} and ${formData.intent2} for ${formData.company}`;
+
+    // Fetch the answer
+    const answer = await get_answer(question);
     // Generate email content
     const content = `
       <p>Dear ${formData.firstname},</p>
-      <p>I hope this email finds you well. I wanted to reach out to discuss how IGEL and Lenovo can help...</p>
-      <!-- Rest of email content -->
+      <p>I hope this email finds you well. I'm reaching out to discuss how IGEL and Lenovo can help address some of the key challenges you may face at ${formData.company} in endpoint management and cloud-connected devices.</p>
+        <p>Some of the key benefits include: </p>
+        <li>${answer}</li>
     `;
 
     // Set the generated email content
@@ -48,7 +82,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome!" />
 
         <div className="form-container">
           {/* Form for input on the left side */}
@@ -57,6 +91,7 @@ export default function Home() {
             <input type="text" name="company" placeholder="Company" required onChange={handleInputChange} />
             <input type="text" name="intent1" placeholder="Intent 1" required onChange={handleInputChange} />
             <input type="text" name="intent2" placeholder="Intent 2" required onChange={handleInputChange} />
+            <input type="text" name="KeyChallenge" placeholder="Key Challenge" required onChange={handleInputChange} />
             {/* Other fields as needed */}
             <button type="submit">Generate Email</button>
           </form>
@@ -83,4 +118,3 @@ export default function Home() {
     </div>
   );
 }
-
